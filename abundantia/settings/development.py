f049c4e9aff979413e490f6df46ffd1345c66ad3
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-07 23:07:14 -0200

    Settings and Project

---
+++ abundantia/settings/development.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'c@vc)dkhlqrutz3y$1y!7^1t_()a(y@$n3z-g@kgw_e$%=goq&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
