6c7e0d83d208dcc843a01e8768805c92b9acbc1c
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-08 19:52:16 -0200

    Added comments for testing.

--- tests/test_currency/test_models.py
+++ tests/test_currency/test_models.py
@@ -9,6 +9,7 @@ pytestmark = pytest.mark.django_db
 
 
 def test_currency_create():
+    # Basic Django Model test
     assert Currency.objects.all().count() == 0
 
     currency = AutoFixture(Currency, generate_fk=True).create_one()
@@ -18,6 +19,7 @@ def test_currency_create():
 
 
 def test_quotations_create():
+    # Basic Django Model test
     assert Quotation.objects.all().count() == 0
 
     quotation = AutoFixture(Quotation, generate_fk=True).create_one()
