8a0b785efc1d010b5fe05b05ec0e329f831cf00d
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-08 19:41:27 -0200

    Test models

---
+++ tests/test_currency/test_models.py
@@ -0,0 +1,27 @@
+import pytest
+
+from autofixture import AutoFixture
+
+from abundantia_api.currency.models import Currency, Quotation
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test_currency_create():
+    assert Currency.objects.all().count() == 0
+
+    currency = AutoFixture(Currency, generate_fk=True).create_one()
+
+    assert currency
+    assert Currency.objects.all().count() == 1
+
+
+def test_quotations_create():
+    assert Quotation.objects.all().count() == 0
+
+    quotation = AutoFixture(Quotation, generate_fk=True).create_one()
+
+    assert quotation
+    assert Quotation.objects.all().count() == 1
+
