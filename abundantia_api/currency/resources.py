16416df3a31e8424f94a6c2456fd7f56e1a1172f
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-11 12:48:19 -0200

    Quotations endpoint for Currency

--- abundantia_api/currency/resources.py
+++ abundantia_api/currency/resources.py
@@ -1,12 +1,27 @@
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
 from abundantia_api.common.viewsets import BaseViewSet
 
-from .serializers import CurrencyReadSerializer
+from .serializers import CurrencyReadSerializer, CurrencyWithQuotationSerializer
 
 from .models import Currency
 
 
 class CurrencyViewSet(BaseViewSet):
-    serializers = {"default": CurrencyReadSerializer}
+    serializers = {
+        "default": CurrencyReadSerializer,
+        "quotations": CurrencyWithQuotationSerializer,
+    }
 
-    queryset = Currency.objects.all()
+    queryset = Currency.objects.prefetch_related("quotations").all()
     http_method_names = ["get", "put", "post"]
+
+    ordering_fields = ("code",)
+    ordering = ("code",)
+
+    @detail_route(methods=["get"], url_path="quotations")
+    def quotations(self, request, pk=None, **kwargs):
+        currency = self.get_object()
+        serializer = self.get_serializer()
+        return Response(serializer.data)
