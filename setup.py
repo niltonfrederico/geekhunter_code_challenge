619467af138dfa4063443674de40aae5d014d9f6
Nilton Frederico Teixeira <niltonfrederico@pm.me>
2019-02-12 15:15:55 -0200

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup_requires = [str(ir.req) for ir in setup_requires]
 
 setup(
     name="abundantia-api",
-    version="1.0.1",
+    version="1.3.0",
     author="Nilton Teixeira",
     author_email="niltonfrederico@gmail.com",
     url="",
